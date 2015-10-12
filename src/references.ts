5e8666a2a9a997122968c01fe5584513e54cc58f
Michele Ursino <michele@micurs.com>
2015-10-12 09:30:50 -0700

    Unified references.ts

---
+++ src/references.ts
@@ -0,0 +1,7 @@
+///<reference path='../typings/node/node.d.ts' />
+///<reference path='../typings/lodash/lodash.d.ts' />
+///<reference path='../typings/q/Q.d.ts' />
+///<reference path='../typings/mime/mime.d.ts' />
+///<reference path='../typings/xml2js/xml2js.d.ts' />
+///<reference path='../typings/bunyan/bunyan.d.ts' />
+///<reference path='../typings/multiparty/multiparty.d.ts' />
